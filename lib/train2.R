cb167899a981965b32fddb9137a0f515e03a4e59
yunyizh <apple@dyn-209-2-235-149.dyn.columbia.edu>
2016-10-22 11:36:58 -0400

    baseline model code

---
+++ lib/train2.R
@@ -0,0 +1,41 @@
+#########################################################
+### Train a classification model with training images ###
+#########################################################
+
+### Author: Yuting Ma
+### Project 3
+### ADS Spring 2016
+
+feature_train<-read.csv("sift_features.csv")
+label_train<-rep(1,2000)
+label_train[1:1000]<-0
+label_train<-as.factor(label_train)
+train <- function(dat_train, label_train, par=NULL){
+feature<-t(feature)
+  ### Train a Gradient Boosting Model (GBM) using processed features from training images
+  
+  ### Input: 
+  ###  -  processed features from images 
+  ###  -  class labels for training images
+  ### Output: training model specification
+  
+  ### load libraries
+  library("gbm")
+  
+  ### Train with gradient boosting model
+  if(is.null(par)){
+    depth <- 3
+  } else {
+    depth <- par$depth
+  }
+  fit_gbm <- gbm.fit(x=dat_train, y=label_train,
+                     n.trees=2000,
+                     distribution="bernoulli",
+                     interaction.depth=depth, 
+                     bag.fraction = 0.5,
+                     verbose=FALSE)
+  best_iter <- gbm.perf(fit_gbm, method="OOB")
+
+  return(list(fit=fit_gbm, iter=best_iter))
+}
+fit_baseline<-train(feature,label_train)
