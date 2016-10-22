87d8840b660cd182b0e2cbe3a1637989b4e70be6
Weichuan Wu <weichuan.wu@columbia.edu>
2016-10-22 14:28:07 -0400

    the baseline model

---
+++ lib/baseline_model.R
@@ -0,0 +1,95 @@
+#########################################################
+### Train a classification model with training images ###
+#########################################################
+
+### Author: Weichuan Wu Credit to Yuting Ma
+### Project 3 Group 4
+### ADS Fall 2016
+
+#clear environment
+rm(list = ls())
+
+#set directory
+setwd("C:/Study/Columbia/W4243_Applied_Data_Science/Project3")
+
+#read data
+sift_feature<-read.csv("sift_features.csv")
+sift_feature<-t(sift_feature)
+
+#create label, 0 represent for chicken and 1 for dog
+label<-rep(1,2000)
+label[1:1000]<-0
+
+#The train process
+train <- function(dat_train, label_train, par=NULL){
+  ### Train a Gradient Boosting Model (GBM) using processed features from training images
+  
+  ### Input: 
+  ###  -  processed features from images 
+  ###  -  class labels for training images
+  ### Output: training model specification
+  
+  ### load libraries
+  library("gbm")
+  
+  ### Train with gradient boosting model
+  if(is.null(par)){
+    depth <- 1
+  } else {
+    depth <- par$depth
+  }
+  fit_gbm <- gbm.fit(x=dat_train, y=label_train,
+                     n.trees=2000,
+                     distribution="bernoulli",
+                     interaction.depth=depth, 
+                     bag.fraction = 0.5,
+                     verbose=TRUE)
+  best_iter <- gbm.perf(fit_gbm, method="OOB")
+  return(list(fit=fit_gbm, iter=best_iter))
+}
+
+#The tset process
+test <- function(fit_train, dat_test){
+  
+  ### Fit the classfication model with testing data
+  
+  ### Input: 
+  ###  - the fitted classification model using training data
+  ###  -  processed features from testing images 
+  ### Output: training model specification
+  
+  ### load libraries
+  library("gbm")
+  
+  pred <- predict(fit_train$fit, newdata=dat_test, 
+                  n.trees=fit_train$iter, type="response")
+  
+  return(as.numeric(pred> 0.5))
+}
+
+#The cross validation process
+cv.function <- function(X.train, y.train, d, K){
+  
+  n <- length(y.train)
+  n.fold <- floor(n/K)
+  s <- sample(rep(1:K, c(rep(n.fold, K-1), n-(K-1)*n.fold)))  
+  cv.error <- rep(NA, K)
+  
+  for (i in 1:K){
+    train.data <- X.train[s != i,]
+    train.label <- y.train[s != i]
+    test.data <- X.train[s == i,]
+    test.label <- y.train[s == i]
+    
+    par <- list(depth=d)
+    fit <- train(train.data, train.label, par)
+    pred <- test(fit, test.data)  
+    cv.error[i] <- mean(pred != test.label)  
+    print(paste(as.character(i/K*100),'%',' completed',sep=''))
+  }			
+  return(c(mean(cv.error),sd(cv.error)))
+  
+}
+
+#The main process
+result<-cv.function(sift_feature,label,1,10)
--- lib/train2.R
+++ lib/train2.R
@@ -6,12 +6,17 @@
 ### Project 3
 ### ADS Spring 2016
 
+rm(list = ls())
+
+#set directory
+setwd("C:/Study/Columbia/W4243_Applied_Data_Science/Project3")
+
 feature_train<-read.csv("sift_features.csv")
 label_train<-rep(1,2000)
 label_train[1:1000]<-0
 label_train<-as.factor(label_train)
+feature<-t(feature_train)
 train <- function(dat_train, label_train, par=NULL){
-feature<-t(feature)
   ### Train a Gradient Boosting Model (GBM) using processed features from training images
   
   ### Input: 
@@ -24,18 +29,39 @@ feature<-t(feature)
   
   ### Train with gradient boosting model
   if(is.null(par)){
-    depth <- 3
+    depth <- 1
   } else {
     depth <- par$depth
   }
   fit_gbm <- gbm.fit(x=dat_train, y=label_train,
-                     n.trees=2000,
+                     n.trees=200,
                      distribution="bernoulli",
                      interaction.depth=depth, 
                      bag.fraction = 0.5,
-                     verbose=FALSE)
+                     verbose=TRUE)
   best_iter <- gbm.perf(fit_gbm, method="OOB")
 
   return(list(fit=fit_gbm, iter=best_iter))
 }
 fit_baseline<-train(feature,label_train)
+
+library("gbm")
+
+### Train with gradient boosting model
+if(is.null(par)){
+  depth <- 1
+} else {
+  depth <- par$depth
+}
+
+fit_gbm <- gbm.fit(x=feature, y=label_train,
+                   n.trees=2000,
+                   distribution="bernoulli",
+                   interaction.depth=depth, 
+                   bag.fraction = 0.5,
+                   verbose=TRUE)
+
+best_iter <- gbm.perf(fit_gbm, method="OOB")
+
+pred <- predict(fit_gbm, newdata=feature[1:5,], 
+                n.trees=best_iter, type="response")
