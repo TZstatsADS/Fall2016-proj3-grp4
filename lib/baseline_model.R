c0e6f12cddb2613ac35568e971942ca471143da6
Weichuan Wu <weichuan.wu@columbia.edu>
2016-10-28 17:24:17 -0400

    calculate error rate using rgb data

--- lib/baseline_model.R
+++ lib/baseline_model.R
@@ -10,7 +10,7 @@
 rm(list = ls())
 
 #set directory
-setwd("C:/Study/Columbia/W4243_Applied_Data_Science/Project3")
+setwd("C:/Study/Columbia/W4243_Applied_Data_Science/Project3/Fall2016-proj3-grp4/data")
 
 #create label, 0 represent for chicken and 1 for dog
 label<-rep(1,2000)
@@ -112,4 +112,26 @@ for(i in 1:k_folds)
 }
 result<-c(mean(cv.error),sd(cv.error))
 
-
+#The cross validation process for rgb pixel data
+#the error rate is 16.7%
+k_folds<-10
+k<-100
+set.seed(1)
+s <- sample(rep(1:10, c(rep(200, 10-1), 2000-(10-1)*200))) 
+d<-3
+cv.error <- rep(NA, k_folds)
+for(i in 1:k_folds)
+{
+  load(paste('rgb_train_',as.character(i),'.RData',sep=''))
+  load(paste('rgb_test_',as.character(i),'.RData',sep=''))
+  train.data<-t(P_train[1:k,])
+  test.data<-t(P_test[1:k,])
+  train.label <- label[s != i]
+  test.label <- label[s == i]
+  par <- list(depth=d)
+  fit <- train(train.data, train.label, par)
+  pred <- test(fit, test.data)  
+  cv.error[i] <- mean(pred != test.label)  
+  print(paste(as.character(i/k_folds*100),'%',' completed',sep=''))
+}
+result<-c(mean(cv.error),sd(cv.error))
--- lib/baseline_model_v2.R
+++ lib/baseline_model_v2.R
@@ -4,7 +4,7 @@
 rm(list = ls())
 
 #set directory
-setwd("C:/Study/Columbia/W4243_Applied_Data_Science/Project3")
+setwd("C:/Study/Columbia/W4243_Applied_Data_Science/Project3/Fall2016-proj3-grp4/data")
 
 #create label, 0 represent for chicken and 1 for dog
 label<-rep(1,2000)
@@ -15,7 +15,7 @@ label[1:1000]<-0
 #####train
 train<-function(dataset_train){
   library(e1071)
-  tune.out=tune(svm ,train.label~.,data=dataset_train ,kernel ='linear',
+  tune.out=tune(svm ,train.label~.,data=dataset_train ,kernel ='radial',
                 ranges =list(cost=c(0.01, 0.1,1)))
   bestmod =tune.out$best.model
   return(bestmod)
@@ -32,7 +32,7 @@ k_folds<-10
 #the pca features number
 k<-20
 set.seed(1)
-s <- sample(rep(1:k_folds, c(rep(200, 10-1), 2000-(10-1)*200))) 
+s <- sample(rep(1:10, c(rep(200, 10-1), 2000-(10-1)*200))) 
 cv.error <- rep(NA, k_folds)
 for(i in 1:k_folds)
 {
@@ -59,12 +59,41 @@ k_folds<-5
 #the pca features number
 k<-100
 set.seed(1)
-s <- sample(rep(1:k_folds, c(rep(200, 10-1), 2000-(10-1)*200))) 
+s <- sample(rep(1:10, c(rep(200, 10-1), 2000-(10-1)*200))) 
 cv.error <- rep(NA, k_folds)
 for(i in 1:k_folds)
 {
   load(paste('pixel_train_',as.character(i),'.RData',sep=''))
   load(paste('pixel_test_',as.character(i),'.RData',sep=''))
+  train.data<-t(P_train[1:k,])
+  test.data<-t(P_test[1:k,])
+  train.label <- label[s != i]
+  test.label <- label[s == i]
+  
+  train.data=data.frame(train.data,train.label=as.factor(train.label))
+  bestmod =train(train.data)
+  colnames(test.data)<-colnames(train.data)[1:k]
+  pred=test(bestmod ,test.data )
+  test.label=as.factor(test.label)
+  cv.error[i] <- mean(pred != test.label)  
+  print(paste(as.character(i/k_folds*100),'%',' completed',sep=''))
+}
+result<-c(mean(cv.error),sd(cv.error))
+
+
+#3.Using rgb pixel as feature, error rate around 14.4%(using linear)
+   #using radial error rate reduce to 12.2%
+#folds for cross validation,can be any interger between 1 and 10
+k_folds<-5
+#the pca features number
+k<-100
+set.seed(1)
+s <- sample(rep(1:10, c(rep(200, 10-1), 2000-(10-1)*200))) 
+cv.error <- rep(NA, k_folds)
+for(i in 1:k_folds)
+{
+  load(paste('rgb_train_',as.character(i),'.RData',sep=''))
+  load(paste('rgb_test_',as.character(i),'.RData',sep=''))
   train.data<-t(P_train[1:k,])
   test.data<-t(P_test[1:k,])
   train.label <- label[s != i]
@@ -151,36 +180,34 @@ for(i in 1:k_folds)
 }
 result<-c(mean(cv.error),sd(cv.error))
 
-################################################################################logistic classifier
-#the accurate rate is around 67%
-#input_data<-data.frame(cbind(pixel_feature,label))
-#input_data<-data.frame(cbind(sift_feature,label))
-#input_data$label<-as.factor(input_data$label)
-#glm.fit=glm(label~. ,data=input_data ,family =binomial )
+#3.Using rgb pixel as feature, error rate around 14%
 
-#only extract the colnmn with p value less than 0.5
-#temp_data<-input_data[,(summary(glm.fit)$coef[,4]<0.05)[2:20]]
-#input_data<-data.frame(cbind(temp_data,label))
-#input_data$label<-as.factor(input_data$label)
+#folds for cross validation,can be any interger between 1 and 10
+k_folds<-10
+#the pca features number
+k<-100
+set.seed(1)
+s <- sample(rep(1:10, c(rep(200, 10-1), 2000-(10-1)*200))) 
+cv.error <- rep(NA, k_folds)
+for(i in 1:k_folds)
+{
+  load(paste('rgb_train_',as.character(i),'.RData',sep=''))
+  load(paste('rgb_test_',as.character(i),'.RData',sep=''))
+  train.data<-t(P_train[1:k,])
+  test.data<-t(P_test[1:k,])
+  train.label <- label[s != i]
+  test.label <- label[s == i]
+  
+  train.data=data.frame(train.data,train.label=as.factor(train.label))
+  fit <- train(train.data)
+  colnames(test.data)<-colnames(train.data)[1:k]
+  pred <- test(fit,test.data)  
+  test.label=as.factor(test.label)
+  cv.error[i] <- mean(pred != test.label)  
+  print(paste(as.character(i/k_folds*100),'%',' completed',sep=''))
+}
+result<-c(mean(cv.error),sd(cv.error))
 
-#accurate_rate<-matrix(data=NA,ncol=10)
-#for(i in 1:10)
-#{
-#set train data and test data
-#  sample_rank<-sample(1:nrow(input_data),0.8*nrow(input_data))
-#  input_train<-input_data[sample_rank,]
-#  input_test<-input_data[-sample_rank,]
-
-#fit the model and calculate the accurate rate
-#  glm.fit=glm(label~. ,data=input_train ,family =binomial )
-#  glm.probs<-predict(glm.fit,input_test,type='response')
-#  glm.pred<-rep(1,nrow(input_test))
-#  glm.pred[glm.probs<0.5]<-0
-#  result=table(pred=as.factor(glm.pred),true=as.factor(input_test$label))
-#  accurate_rate[i]<-(result[1,1]+result[2,2])/sum(result)
-#  print(paste(as.character(i/10*100),'%',' completed',sep=''))
-#}
-#mean(accurate_rate)
 
 #####################################################################################LDA
 #1.Using PCA processed SIFT features as feature, error rate around 33.1%.
@@ -250,6 +277,33 @@ for(i in 1:k_folds)
 }
 result<-c(mean(cv.error),sd(cv.error))
 
+#3.Using rgb pixel as feature, error rate around 15.1%
+#folds for cross validation,can be any interger between 1 and 10
+k_folds<-10
+#the pca features number
+k<-100
+set.seed(1)
+s <- sample(rep(1:10, c(rep(200, 10-1), 2000-(10-1)*200))) 
+cv.error <- rep(NA, k_folds)
+for(i in 1:k_folds)
+{
+  load(paste('rgb_train_',as.character(i),'.RData',sep=''))
+  load(paste('rgb_test_',as.character(i),'.RData',sep=''))
+  train.data<-t(P_train[1:k,])
+  test.data<-t(P_test[1:k,])
+  train.label <- label[s != i]
+  test.label <- label[s == i]
+  
+  train.data=data.frame(train.data,train.label=as.factor(train.label))
+  fit <- train(train.data)
+  colnames(test.data)<-colnames(train.data)[1:k]
+  pred <- test(fit,as.data.frame(test.data))  
+  test.label=as.factor(test.label)
+  cv.error[i] <- mean(pred != test.label)  
+  print(paste(as.character(i/k_folds*100),'%',' completed',sep=''))
+}
+result<-c(mean(cv.error),sd(cv.error))
+
 #####################################################################################QDA
 #1.Using PCA processed SIFT features as feature, error rate around 39%.
 train<-function(dataset_train){
@@ -302,6 +356,33 @@ for(i in 1:k_folds)
 {
   load(paste('pixel_train_',as.character(i),'.RData',sep=''))
   load(paste('pixel_test_',as.character(i),'.RData',sep=''))
+  train.data<-t(P_train[1:k,])
+  test.data<-t(P_test[1:k,])
+  train.label <- label[s != i]
+  test.label <- label[s == i]
+  
+  train.data=data.frame(train.data,train.label=as.factor(train.label))
+  fit <- train(train.data)
+  colnames(test.data)<-colnames(train.data)[1:k]
+  pred <- test(fit,as.data.frame(test.data))  
+  test.label=as.factor(test.label)
+  cv.error[i] <- mean(pred != test.label)  
+  print(paste(as.character(i/k_folds*100),'%',' completed',sep=''))
+}
+result<-c(mean(cv.error),sd(cv.error))
+
+#3.Using rgb pixel as feature, error rate around 23.9%
+#folds for cross validation,can be any interger between 1 and 10
+k_folds<-10
+#the pca features number
+k<-100
+set.seed(1)
+s <- sample(rep(1:10, c(rep(200, 10-1), 2000-(10-1)*200))) 
+cv.error <- rep(NA, k_folds)
+for(i in 1:k_folds)
+{
+  load(paste('rgb_train_',as.character(i),'.RData',sep=''))
+  load(paste('rgb_test_',as.character(i),'.RData',sep=''))
   train.data<-t(P_train[1:k,])
   test.data<-t(P_test[1:k,])
   train.label <- label[s != i]
